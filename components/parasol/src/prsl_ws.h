6ffff2219fcb52883d85fc12c90397368c87ab5a
Marcel Jira <marcel@example.local>
2026-07-12 18:49:03 +0200

    rewrite: prsl_ws.h — rename symbols

--- components/parasol/src/prsl_ws.h
+++ components/parasol/src/prsl_ws.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "ESPAsyncWebServer.h"
-#include "pwui_store.h"
+#include "prsl_store.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-esp_err_t pwui_ws_init(AsyncWebSocket *ws, pwui_store_t *store);
+esp_err_t prsl_ws_init(AsyncWebSocket *ws, prsl_store_t *store);
 
 #ifdef __cplusplus
 }
